286bb4d760907b0825b3da8643b5aadbfe41a509
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-10-27 19:22:26 +0200

    React Router installed and App component recreated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path='/' element={}/>
+      <Route path='/cards' element={}/>
+    </Routes>
   );
 }
 
